52463c66a845cd79db9bcc9d526af38f75bc2676
Sergio Rangel <142181326+231273214@users.noreply.github.com>
2025-09-05 15:54:42 -0500

    Se subio el proyecto

---
+++ Assets/scripts/CambioEscena.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CambioEscena : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OtraEscena()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(1);
+    }
+
+}
+
